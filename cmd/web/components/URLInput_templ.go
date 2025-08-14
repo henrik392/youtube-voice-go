73f94e960ee9d9fec5b422d1e9998beb398708ec
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-08-14 15:37:25 +0200

    Trying to fix validation

--- cmd/web/components/URLInput_templ.go
+++ cmd/web/components/URLInput_templ.go
@@ -36,7 +36,7 @@ func URLInput(isValid bool, url string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"url-input\"><label for=\"url\" class=\"block pl-4 text-sm font-bold leading-6 text-gray-900\">Video URL</label><div class=\"relative mt-2 rounded-md shadow-sm\"><div class=\"absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none\"><img class=\"w-auto h-4 opacity-70\" src=\"https://cdn-icons-png.freepik.com/256/455/455691.png?semt=ais_hybrid\" alt=\"\"></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"url-input\"><label for=\"url\" class=\"block pl-4 text-sm font-bold leading-6 text-gray-900\">Video URL</label><div class=\"relative mt-2 rounded-md shadow-sm\"><div class=\"flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none\"><img class=\"w-auto h-4 opacity-70\" src=\"https://cdn-icons-png.freepik.com/256/455/455691.png?semt=ais_hybrid\" alt=\"\"></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -58,30 +58,56 @@ func URLInput(isValid bool, url string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" placeholder=\"https://www.youtube.com/watch?v=...\" value=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" data-valid=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var4 string
-		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(url)
+		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(isValid)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/components/URLInput.templ`, Line: 25, Col: 15}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/components/URLInput.templ`, Line: 24, Col: 24}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "\" hx-trigger=\"keyup changed delay:100ms\" hx-post=\"/validate-url\" hx-target=\"#url-input\" hx-swap=\"outerHTML\" hx-indicator=\"\"></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "\" aria-invalid=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var5 string
+		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(!isValid)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/components/URLInput.templ`, Line: 25, Col: 27}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "\" placeholder=\"https://www.youtube.com/watch?v=...\" value=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var6 string
+		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(url)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/components/URLInput.templ`, Line: 27, Col: 15}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "\" hx-trigger=\"keyup changed delay:100ms\" hx-post=\"/validate-url\" hx-target=\"#url-input\" hx-swap=\"outerHTML\" hx-indicator=\"\"></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		if isValid {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<div id=\"processing-status\" class=\"mt-2 text-sm text-blue-600\"><div class=\"flex items-center\"><svg class=\"animate-spin ml-3 mr-3 h-4 w-4 text-blue-600\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\"><circle class=\"opacity-25\" cx=\"12\" cy=\"12\" r=\"10\" stroke=\"currentColor\" stroke-width=\"4\"></circle> <path class=\"opacity-75\" fill=\"currentColor\" d=\"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z\"></path></svg> Processing video...</div><script>\n\t\t\t\t\t// Trigger video processing when valid URL is detected\n\t\t\t\t\thtmx.ajax('POST', '/process-video', {\n\t\t\t\t\t\ttarget: '#processing-status',\n\t\t\t\t\t\tswap: 'innerHTML',\n\t\t\t\t\t\tvalues: { url: document.getElementById('url').value }\n\t\t\t\t\t});\n\t\t\t\t</script></div>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<div id=\"processing-status\" class=\"mt-2 text-sm text-blue-600\"><div class=\"flex items-center\"><svg class=\"mr-3 ml-3 w-4 h-4 text-blue-600 animate-spin\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\"><circle class=\"opacity-25\" cx=\"12\" cy=\"12\" r=\"10\" stroke=\"currentColor\" stroke-width=\"4\"></circle> <path class=\"opacity-75\" fill=\"currentColor\" d=\"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z\"></path></svg> Processing video...</div><script>\n\t\t\t\t\t// Trigger video processing when valid URL is detected\n\t\t\t\t\thtmx.ajax('POST', '/process-video', {\n\t\t\t\t\t\ttarget: '#processing-status',\n\t\t\t\t\t\tswap: 'innerHTML',\n\t\t\t\t\t\tvalues: { url: document.getElementById('url').value }\n\t\t\t\t\t});\n\t\t\t\t</script></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "</div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
